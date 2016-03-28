2bfec1da639e81dbb89493dcf05478ce1ee8585e
Waqqas Jabbar <waqqas.jabbar@roadez.com>
2016-03-28 12:15:03 +0500

    Added Taplytics

--- app/src/main/java/com/example/mbella/helloworldapp/MainActivity.java
+++ app/src/main/java/com/example/mbella/helloworldapp/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.mbella.helloworldapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.taplytics.sdk.Taplytics;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Insert the following line of code to initialize the Taplytics SDK
+        Taplytics.startTaplytics(this, "1d60abdc9953ab49c851cb247782ad98a8732482");
+
+
         setContentView(R.layout.activity_main);
     }
 }
